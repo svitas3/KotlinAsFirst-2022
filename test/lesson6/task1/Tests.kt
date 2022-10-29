44975afcfa96b8adc9f734fc4c70be468faeec4d
Svit.as <lyapinasv@edu.spbstu.ru>
2022-10-30 01:50:07 +0300

    fixed bugs in 3, 4 and 5 lessons

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -31,17 +31,17 @@ class Tests {
         assertEquals("23:59:59", timeSecondsToStr(86399))
     }
 
-//    @Test
-//    @Tag("4")
-//    fun dateStrToDigit() {
-//        assertEquals("15.07.2016", dateStrToDigit("15 июля 2016"))
-//        assertEquals("", dateStrToDigit("3 мартобря 1918"))
-//        assertEquals("18.11.2018", dateStrToDigit("18 ноября 2018"))
-//        assertEquals("", dateStrToDigit("23"))
-//        assertEquals("03.04.2011", dateStrToDigit("3 апреля 2011"))
-//        assertEquals("", dateStrToDigit("32 сентября 2011"))
-//        assertEquals("", dateStrToDigit("29 февраля 1993"))
-//    }
+    @Test
+    @Tag("4")
+    fun dateStrToDigit() {
+        assertEquals("15.07.2016", dateStrToDigit("15 июля 2016"))
+        assertEquals("", dateStrToDigit("3 мартобря 1918"))
+        assertEquals("18.11.2018", dateStrToDigit("18 ноября 2018"))
+        assertEquals("", dateStrToDigit("23"))
+        assertEquals("03.04.2011", dateStrToDigit("3 апреля 2011"))
+        assertEquals("", dateStrToDigit("32 сентября 2011"))
+        assertEquals("", dateStrToDigit("29 февраля 1993"))
+    }
 
     @Test
     @Tag("4")
